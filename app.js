52e7fd690a2bf341a36101a551aba1cfb7c58be9
avisek123 <mrperfectavi89@gmail.com>
2021-07-05 22:23:00 +0530

    get data from fake api

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.send("Hi from Contact");
 });
+app.get("/data", (req, res) => {
+  res.send("https://jsonplaceholder.typicode.com/todos/1");
+});
 app.listen(4000, () => {
   console.log("Listening the port from 4000");
 });
